35f8816b938520dd55a7058652b4dd5a42d93663
Hwijoon Lim <wjuni@kaist.ac.kr>
2020-09-29 03:01:21 +0900

    first commit

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
+
+cd /tmp
+wget https://github.com/prometheus/node_exporter/releases/download/v1.0.1/node_exporter-1.0.1.linux-amd64.tar.gz
+tar -xvzf node_exporter-1.0.1.linux-amd64.tar.gz
+sudo mv node_exporter-1.0.1.linux-amd64/node_exporter /usr/local/bin
+
+sudo useradd -s /sbin/nologin node_exporter
+sudo systemctl daemon-reload
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
+sudo systemctl status node_exporter
