4346269ee1eb5b554ce803fe89b59e3821c0ddcc
Hwijoon Lim <wjuni@kaist.ac.kr>
2022-09-14 13:30:31 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo cp node_exporter.service /etc/systemd/system/node_exporter.service
 cd /tmp
 wget https://github.com/prometheus/node_exporter/releases/download/v1.3.1/node_exporter-1.3.1.linux-amd64.tar.gz
 tar -xvzf node_exporter-1.3.1.linux-amd64.tar.gz
+sudo mkdir -p /usr/local/bin
 sudo mv node_exporter-1.3.1.linux-amd64/node_exporter /usr/local/bin
 
 sudo useradd -s /sbin/nologin node_exporter
